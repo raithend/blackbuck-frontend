3bcec5ebd7830fad89e56f7003a70853231786aa
raithend <37509998+raithend@users.noreply.github.com>
2024-08-18 16:50:07 +0900

    emailログインのためのクレデンシャルの部分を追加した。

--- auth.ts
+++ auth.ts
@@ -1,11 +1,40 @@
 import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
 import Facebook from "next-auth/providers/facebook"
 import Google from "next-auth/providers/google"
 import Twitter from "next-auth/providers/twitter"
 import GitHub from "next-auth/providers/github"
+import { saltAndHashPassword } from "@/utils/password"
 
 export const config = {
-  providers: [Facebook, Google, Twitter, GitHub],
+  providers: [Facebook, Google, Twitter, GitHub, 
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null
+ 
+        // logic to salt and hash password
+        const pwHash = saltAndHashPassword(credentials.password)
+ 
+        // logic to verify if the user exists
+        user = await getUserFromDb(credentials.email, pwHash)
+ 
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error("User not found.")
+        }
+ 
+        // return user object with their profile data
+        return user
+      },
+    })
+  ],
   pages: {
     signIn: "/login", 
     signOut: "/logout",
