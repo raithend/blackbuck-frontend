b1e62d5ed9e86ee4628404c1017264d7d2a68e3d
Yoshiki Takeshita <37509998+raithend@users.noreply.github.com>
2024-08-23 17:55:56 +0900

    Update auth.ts Google provider

--- auth.ts
+++ auth.ts
@@ -6,7 +6,15 @@ import Twitter from "next-auth/providers/twitter"
 import GitHub from "next-auth/providers/github"
 
 export const config = {
-  providers: [Facebook, Google, Twitter, GitHub],
+  providers: [
+    Facebook, 
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Twitter, 
+    GitHub
+  ],
   pages: {
     signIn: "/sign-in", 
     signOut: "/sign-out",
