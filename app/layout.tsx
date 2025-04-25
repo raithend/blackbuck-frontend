9a9ca6b04ccbfcdb6ca9d0dfc1ba6c266893d425
Yoshiki Takeshita <raithend@gmail.com>
2025-04-25 20:50:28 +0900

    Delete suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
 					defaultTheme="system"
 					enableSystem
 					disableTransitionOnChange
-					suppressHydrationWarning
 				>
 					<SupabaseAuthProvider>
 						<ProfileProvider>
