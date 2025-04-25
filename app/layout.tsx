e6d20e0e64546425fa7884cd82604c9e300de698
Yoshiki Takeshita <raithend@gmail.com>
2025-04-25 20:17:04 +0900

    Fix hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,13 +16,14 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="ja">
-			<body className={inter.className}>
+		<html lang="ja" suppressHydrationWarning>
+			<body className={inter.className} suppressHydrationWarning>
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
 					enableSystem
 					disableTransitionOnChange
+					suppressHydrationWarning
 				>
 					<SupabaseAuthProvider>
 						{children}
