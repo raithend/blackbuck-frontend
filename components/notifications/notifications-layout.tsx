b761d4fd6a180e0891b425ce7b7feafe5535b0a0
Yoshiki Takeshita <raithend@gmail.com>
2025-04-26 22:39:47 +0900

    Delete h1 tag in notifications and settings page

--- components/notifications/notifications-layout.tsx
+++ components/notifications/notifications-layout.tsx
@@ -5,28 +5,25 @@ import { NotificationList } from "./notification-list";
 
 export function NotificationsLayout() {
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-2xl font-bold mb-8">通知</h1>
-      <Tabs defaultValue="all" className="w-full">
-        <TabsList className="grid w-full grid-cols-4">
-          <TabsTrigger value="all">すべて</TabsTrigger>
-          <TabsTrigger value="follows">フォロー</TabsTrigger>
-          <TabsTrigger value="likes">いいね</TabsTrigger>
-          <TabsTrigger value="comments">コメント</TabsTrigger>
-        </TabsList>
-        <TabsContent value="all">
-          <NotificationList type="all" />
-        </TabsContent>
-        <TabsContent value="follows">
-          <NotificationList type="follows" />
-        </TabsContent>
-        <TabsContent value="likes">
-          <NotificationList type="likes" />
-        </TabsContent>
-        <TabsContent value="comments">
-          <NotificationList type="comments" />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <Tabs defaultValue="all" className="w-full">
+      <TabsList className="grid w-full grid-cols-4">
+        <TabsTrigger value="all">すべて</TabsTrigger>
+        <TabsTrigger value="follows">フォロー</TabsTrigger>
+        <TabsTrigger value="likes">いいね</TabsTrigger>
+        <TabsTrigger value="comments">コメント</TabsTrigger>
+      </TabsList>
+      <TabsContent value="all">
+        <NotificationList type="all" />
+      </TabsContent>
+      <TabsContent value="follows">
+        <NotificationList type="follows" />
+      </TabsContent>
+      <TabsContent value="likes">
+        <NotificationList type="likes" />
+      </TabsContent>
+      <TabsContent value="comments">
+        <NotificationList type="comments" />
+      </TabsContent>
+    </Tabs>
   );
 } 
--- components/settings/settings-layout.tsx
+++ components/settings/settings-layout.tsx
@@ -7,24 +7,21 @@ import { HelpSupport } from "./help-support";
 
 export function SettingsLayout() {
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-2xl font-bold mb-8">設定</h1>
-      <Tabs defaultValue="account" className="w-full">
-        <TabsList className="grid w-full grid-cols-3">
-          <TabsTrigger value="account">アカウント</TabsTrigger>
-          <TabsTrigger value="security">セキュリティ</TabsTrigger>
-          <TabsTrigger value="help">ヘルプ</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          <AccountSettings />
-        </TabsContent>
-        <TabsContent value="security">
-          <SecuritySettings />
-        </TabsContent>
-        <TabsContent value="help">
-          <HelpSupport />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <Tabs defaultValue="account" className="w-full">
+      <TabsList className="grid w-full grid-cols-3">
+        <TabsTrigger value="account">アカウント</TabsTrigger>
+        <TabsTrigger value="security">セキュリティ</TabsTrigger>
+        <TabsTrigger value="help">ヘルプ</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <AccountSettings />
+      </TabsContent>
+      <TabsContent value="security">
+        <SecuritySettings />
+      </TabsContent>
+      <TabsContent value="help">
+        <HelpSupport />
+      </TabsContent>
+    </Tabs>
   );
 } 
