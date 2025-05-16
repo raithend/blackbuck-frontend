91186c8597c3e60e91d40fbc7c20c0d0f38bf128
Yoshiki Takeshita <raithend@gmail.com>
2025-05-16 19:30:47 +0900

    Delete next.config.ts and merge its contents into next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,28 @@ const nextConfig = {
     });
     return config;
   },
+  async rewrites() {
+    const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${apiBaseUrl}/api/:path*`,
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig; 
