bb969bde3e110ac8b698db19c8ef9fe1494c6042
Yoshiki Takeshita <raithend@gmail.com>
2025-05-16 19:31:07 +0900

    Delete lib/supabase.ts

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-// このファイルは互換性のために残しています
-// 新しいコードでは、supabase-browser.tsまたはsupabase-server.tsを直接インポートしてください
-
-export { createBrowserClient } from './supabase-browser'
-// サーバーコンポーネントでのみ使用可能
-// export { createServerSupabaseClient } from './supabase-server' 
