133fd247c3e9e13485c3b5efd0aeb1ebd79308f7
Yoshiki Takeshita <raithend@gmail.com>
2025-05-18 23:11:27 +0900

    Delete unnecessary file and component

--- components/header.tsx
+++ components/header.tsx
@@ -2,12 +2,10 @@
 
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useUser } from '@/contexts/user-context';
 import { UserAuthButton } from '@/components/auth/user-auth-button';
 import { PostButton } from '@/components/post/post-button';
-import { TestCombobox } from '@/components/post/text-combobox';
 
 export function Header() {
   const { userProfile, isLoading } = useUser();
@@ -29,19 +27,6 @@ export function Header() {
           </nav>
         </div>
         <PostButton />
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="outline">
-              test
-            </Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>新規投稿</DialogTitle>
-              <TestCombobox />
-            </DialogHeader>
-          </DialogContent>
-        </ Dialog>
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-2">
             {isLoading ? (
