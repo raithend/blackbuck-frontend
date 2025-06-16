9bb6129d9619f4face4d745bd0e636b57f2f51b4
Yoshiki Takeshita <raithend@gmail.com>
2025-06-16 23:06:20 +0900

    Commented out likes page components

--- app/(app)/users/[accountId]/likes/page.tsx
+++ app/(app)/users/[accountId]/likes/page.tsx
@@ -2,25 +2,27 @@
 
 import { PostCards } from "@/app/components/post/post-cards";
 import { fetcher } from "@/app/lib/fetcher";
-import { Post } from "@/app/api/db/types";
+import { Post } from "@/app/types/types";
 import useSWR from "swr";
 
-export default function LikesPage({ params }: { params: { id: string } }) {
-	const {
-		data: posts,
-		error,
-		isLoading,
-	} = useSWR<Post[]>(`/api/users/${params.id}/likes`, fetcher);
+export default function LikesPage() {
+// export default function LikesPage({ params }: { params: { id: string } }) {
+	// const {
+	// 	data: posts,
+	// 	error,
+	// 	isLoading,
+	// } = useSWR<Post[]>(`/api/users/${params.id}/likes`, fetcher);
 
-	if (error) {
-		return <div>エラーが発生しました</div>;
-	}
+	// if (error) {
+	// 	return <div>エラーが発生しました</div>;
+	// }
 
-	if (isLoading) {
-		return <div>読み込み中...</div>;
-	}
+	// if (isLoading) {
+	// 	return <div>読み込み中...</div>;
+	// }
 
 	return (
-		<PostCards posts={posts || []} />
+		// <PostCards posts={posts || []} />
+		<div>likes</div>
 	);
 }
