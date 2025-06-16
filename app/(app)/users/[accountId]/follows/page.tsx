c35f31596c9e3e09e008c78bd24429e283599389
Yoshiki Takeshita <raithend@gmail.com>
2025-06-16 22:59:32 +0900

    Commented out follows page logics

--- app/(app)/users/[accountId]/follows/page.tsx
+++ app/(app)/users/[accountId]/follows/page.tsx
@@ -5,25 +5,25 @@ import { User } from "@/app/types/types";
 import useSWR from "swr";
 
 export default function FollowsPage({ params }: { params: { id: string } }) {
-	const {
-		data: follows,
-		error,
-		isLoading,
-	} = useSWR<User[]>(`/api/users/${params.id}/follows`, fetcher);
+	// const {
+	// 	data: follows,
+	// 	error,
+	// 	isLoading,
+	// } = useSWR<User[]>(`/api/users/${params.id}/follows`, fetcher);
 
-	if (error) {
-		return <div>エラーが発生しました</div>;
-	}
+	// if (error) {
+	// 	return <div>エラーが発生しました</div>;
+	// }
 
-	if (isLoading) {
-		return <div>読み込み中...</div>;
-	}
+	// if (isLoading) {
+	// 	return <div>読み込み中...</div>;
+	// }
 
 	return (
 		<div>
-			{follows?.map((user) => (
+			{/* {follows?.map((user) => (
 				<div key={user.id}>{user.username}</div>
-			))}
+			))} */}
 		</div>
 	);
 }
